3ff37d2318487e45694bfd33f9e78ca9bda7bcc0
schessw <42581659+schessw@users.noreply.github.com>
2018-08-27 04:21:22 +0800

    Update createPiece() Method

--- PieceFactory.java
+++ PieceFactory.java
@@ -1,12 +1,25 @@
+package myrmidonChess;
+
 public class PieceFactory{
     public Piece createPiece(String PieceType, String PieceColor){
-        Piece newPiece = null;
-        if(PieceType.equalsIgnoreCase("Triangle") & PieceColor.equalsIgnoreCase("Red")){
+        
+        if (PieceType.equalsIgnoreCase("Triangle") & PieceColor.equalsIgnoreCase("Red")){
             return new Triangle("Red");
-        } 
-        else if(PieceType.equalsIgnoreCase("Triangle") & PieceColor.equalsIgnoreCase("Blue")){
+        } else if (PieceType.equalsIgnoreCase("Triangle") & PieceColor.equalsIgnoreCase("Blue")){
             return new Triangle("Blue");
-        }else{
+        } else if (PieceType.equalsIgnoreCase("Sun") & PieceColor.equalsIgnoreCase("Red")){
+            return new Sun("Red");
+        } else if (PieceType.equalsIgnoreCase("Sun") & PieceColor.equalsIgnoreCase("Blue")){
+            return new Sun("Blue");
+        } else if (PieceType.equalsIgnoreCase("Chervon") & PieceColor.equalsIgnoreCase("Red")){
+            return new Chervon("Red");
+        } else if (PieceType.equalsIgnoreCase("Chervon") & PieceColor.equalsIgnoreCase("Blue")){
+            return new Chervon("Blue");
+        } else if (PieceType.equalsIgnoreCase("Plus") & PieceColor.equalsIgnoreCase("Red")){
+            return new Plus("Red");
+        } else if (PieceType.equalsIgnoreCase("Plus") & PieceColor.equalsIgnoreCase("Blue")){
+            return new Plus("Blue");    
+        } else {
             return null;
         }
     }
