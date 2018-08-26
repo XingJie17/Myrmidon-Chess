70ef47d96d3f929ea60ecad25a26c15e9a1c62d1
schessw <42581659+schessw@users.noreply.github.com>
2018-08-27 05:12:42 +0800

    Update showMove() method
    Update showMove() method, but still need to check if blocked by pieces of same color.

--- Plus.java
+++ Plus.java
@@ -1,11 +1,14 @@
-//package MyrmidonChess;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Random;
+package myrmidonChess;
+
+/** NOT DONE **/
+
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.util.Random;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.io.*;
+//import java.util.Arrays;
+//import java.io.*;
 
 public class Plus extends Piece  
 {
@@ -28,39 +31,56 @@ public class Plus extends Piece
     public void setColor(String color){
         this.color = color;
     }
+    
+    public void checkOccupied(ArrayList<Integer> a) {
+    	// TODO
+    }
+    
 	public ArrayList<Integer> showMove(int r,int c)
 	{
+		ArrayList<Integer> a = new ArrayList<Integer>();
 		//Haven't done (if blockage then cannot move)[PIECE]
 		//Verify pawn can go backwards direction? 
-		for(int step=1;step<=2;step++)
-		{
+		
+		
+		for(int step = 1; step<=2; step++){
+			
+			// Vertical movement
 			int r1 = 0;
             int c1 = 0;
             int r2 = 0;
             int c2 = 0;
             r1 = r + step;
-            c1 = c + step;
+            c1 = c;
             r2 = r - step;
-            c2 = c - step;
-            if(c1>=0 || c1<=6)
-			{
-                a.add(r);
+            c2 = c;
+            if((r1>=0 && r1<=5) && (c1>=0 & c1<=6)){
+                a.add(r1);
                 a.add(c1);
             }
-            if(c2>=0 || c2<=6)
-			{
-                a.add(r);
+            if((r2>=0 && r2<=5) && (c2>=0 && c2<=6)){
+                a.add(r2);
                 a.add(c2);
             }
-			 if (r1>=0 || r1<=5){
-                a.add(r1);
-                a.add(c);
-			}
-            if(r2>=0 || r2<=5){
-                a.add(r2);
-                a.add(c);
-            }		
-		}
+            
+            // Horizontal movement
+            int r3 = 0;
+            int c3 = 0;
+            int r4 = 0;
+            int c4 = 0;
+            r3 = r;
+            c3 = c + step;
+            r4 = r;
+            c4 = c - step;
+            if((r3>=0 && r3<=5) && (c3>=0 & c3<=6)){
+                a.add(r3);
+                a.add(c3);
+            }
+            if((r4>=0 && r4<=5) && (c4>=0 && c4<=6)){
+                a.add(r4);
+                a.add(c4);
+            }
+		}	//end SHOWMOVE
 		return a;
-	}//end SHOWMOVE
+	}
 }
--- Sun.java
+++ Sun.java
@@ -1,12 +1,15 @@
-//package MyrmidonChess;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Random;
+package myrmidonChess;
+
+/** NOT DONE **/
+
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.util.Random;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.io.*;
-public class Sun extends  Piece  
+//import java.util.Arrays;
+//import java.io.*;
+public class Sun extends Piece  
 {
 	private String type;
     private String color;
@@ -30,60 +33,74 @@ public class Sun extends  Piece
 	{
         this.color = color;
     }
+    
+    
 	public ArrayList<Integer> showMove(int r,int c)
 	{
-		//Haven't done (if blockage then cannot move)[PIECE]		
-			int r1 = 0;
-			int c1 = 0;
-			int r2 = 0;
-            int c2 = 0;
-			r1 = r + step;
-            c1 = c + step;
-			r2 = r - step;
-            c2 = c - step;
-			
-			if(r1>=0 || r1<=5) 
-			{
-                a.add(r1);				
-				if(c1>=0 || c1<=6)
-				{
-					 a.add(c1);
-				}
-				else if(c2>=0 || c2<=6)
-				{
-					 a.add(c2);
-				}
-				else
-				{
-					a.add(c);
-				}               
-            }
-			if(c1>=0 || c1<=6)
-			{
-				a.add(r);
-				a.add(c1);
-			}	
-			if(c2>=0 || c2<=6)
-			{
-				a.add(r);
-				a.add(c2);
-			}
-			if(r2>=0 || r2<=5)
-			{
-				a.add(r2);
-				if (c1>=0 || c1<=6)
-				{
-					a.add(c1);
-				}
-				else if(c2>=0 || c2<=6)
-				{               
-                a.add(c2);
-				}
-				else
-				{
-					a.add(c);	
-				}	
-			}
-			return a;
-	}//end SHOWMOVE
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		
+		// Vertical movement
+		int r1 = 0;
+        int c1 = 0;
+        int r2 = 0;
+        int c2 = 0;
+        r1 = r + 1;
+        c1 = c;
+        r2 = r - 1;
+        c2 = c;
+        if((r1>=0 && r1<=5) && (c1>=0 & c1<=6)){
+            a.add(r1);
+            a.add(c1);
+        }
+        if((r2>=0 && r2<=5) && (c2>=0 && c2<=6)){
+            a.add(r2);
+            a.add(c2);
+        }
+        
+        // Horizontal movement
+        int r3 = 0;
+        int c3 = 0;
+        int r4 = 0;
+        int c4 = 0;
+        r3 = r;
+        c3 = c + 1;
+        r4 = r;
+        c4 = c - 1;
+        if((r3>=0 && r3<=5) && (c3>=0 & c3<=6)){
+            a.add(r3);
+            a.add(c3);
+        }
+        if((r4>=0 && r4<=5) && (c4>=0 && c4<=6)){
+            a.add(r4);
+            a.add(c4);
+        }
+        
+        // Diagonal movement
+        int r5 = 0;
+        int c5 = 0;
+        int r6 = 0;
+        int c6 = 0;
+        r5 = r + 1;
+        c5 = c + 1;
+        r6 = r - 1;
+        c6 = c - 1;
+        if((r5>=0 && r5<=5) && (c5>=0 & c5<=6)){
+            a.add(r5);
+            a.add(c5);
+        }
+        if((r5>=0 && r5<=5) && (c6>=0 & c6<=6)){
+            a.add(r5);
+            a.add(c6);
+        }
+        if((r6>=0 && r6<=5) && (c5>=0 && c5<=6)){
+            a.add(r6);
+            a.add(c5);
+        }
+        if((r6>=0 && r6<=5) && (c6>=0 && c6<=6)){
+            a.add(r6);
+            a.add(c6);
+        }
+        
+        return a;
+	}
 }
