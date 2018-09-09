9eefc2368f21e204c11208c6ac328f3c3491938b
Lucinda17 <lcx.lucinda@gmail.com>
2018-09-09 23:48:23 +0800

    completed move piece

--- Board.java
+++ Board.java
@@ -34,39 +34,96 @@ public final class Board extends JPanel{
 	private static JButton[][] grid = new JButton[ROWS][COLUMNS];
 	private static Piece[][] pieceManager = new Piece[ROWS][COLUMNS];
 	private static PieceFactory pieceFactory = new PieceFactory();
+	private static int turn= 0;
+	//to store the buttons that a piece can move to 
+	private static ArrayList<Integer> availableBtn = new ArrayList<Integer>(); 
+	//the store previous pieces which is EmptyWhite
+	private static Piece prevPiece=pieceFactory.createPiece("Empty","White");
+	private static int prevRow=0;
+	private static int prevCol=0;
+	private static int j=0;
+	private static int clickOnBP = 0;
 	
 	public static JPanel insertPanel() {
 		
 		// create grid layout on the chess board
 		board.setLayout(new GridLayout(ROWS, COLUMNS, 1, 1));
 		board.setSize(200,200);
+		
 		// assign color to each grid
-		for (int r = 0; r < ROWS; r++) {
-			for (int c = 0; c < COLUMNS;  c++) {
+		for (int r = 0; r < ROWS; r++) 
+		{
+			for (int c = 0; c < COLUMNS;  c++) 
+			{
 				String btnName = r+","+c;
 				// Create a temporary single grid to be colored
 				JButton g = new JButton();
 				g.setActionCommand(btnName);
 				g.addActionListener(new ActionListener()
 				{
-				    public void actionPerformed(ActionEvent e){
+				    public void actionPerformed(ActionEvent e)
+					{
 						resetBoardColor();
 						String temp = g.getActionCommand();
 						int row = Integer.parseInt(temp.substring(0,1));
 						int col = Integer.parseInt(temp.substring(2));
-						if(pieceManager[row][col] != null){
-							ArrayList<Integer> availableBtn = new ArrayList<Integer>(); 
-							availableBtn = pieceManager[row][col].showMove(row,col);
-							for(int i = 0; i < availableBtn.size(); i++){
-								int x = i;
-								int y = i+1;
-								i++;
-								//System.out.println("i: "+i);
-								if(pieceManager[availableBtn.get(x)][availableBtn.get(y)] == null){
-									grid[availableBtn.get(x)][availableBtn.get(y)].setBackground(Color.GREEN);
+						
+						
+						if(pieceManager[row][col].getType() != "Empty")
+						{
+                            if(pieceManager[row][col].getColor().equalsIgnoreCase(getPlayerTurn()))
+							{
+								prevRow = row;
+								prevCol = col;
+                                setTurnFromBoard();
+								turn++;
+								prevPiece=pieceManager[row][col];
+								for(int i = 0; i < availableBtn.size(); i++)
+								{
+						        	int x = i;
+						        	int y = i+1;
+						        	i++;
 								}
-								//System.out.println("r: "+availableBtn.get(x)+" c: "+availableBtn.get(y));
+								Piece newPiece = pieceFactory.createPiece(pieceManager[row][col].getType(),pieceManager[row][col].getColor());
+								availableBtn = newPiece.showMove(row,col);
+								//show the available move of piece in green 
+						        for(int i = 0; i < availableBtn.size(); i++)
+								{
+						        	int x = i;
+						        	int y = i+1;
+						        	i++;
+									if(pieceManager[availableBtn.get(x)][availableBtn.get(y)].getType() == "Empty")
+									{
+						        		grid[availableBtn.get(x)][availableBtn.get(y)].setBackground(Color.GREEN);
+						        	}
+								}
+								setTurnFromBoard();
+                            }
+							
+						}
+						
+						//if click on empty spot
+						else
+						{
+							String color=prevPiece.getColor();
+							String type=prevPiece.getType();
+							String fileName=color+type+".png";
+							for(int i=0;i<availableBtn.size();i++)
+							{
+								int x=i;
+								int y=i+1;
+								i++;
+								if(row==availableBtn.get(x) &&col==availableBtn.get(y))
+								{
+									pieceManager[row][col].setType(type);
+									pieceManager[row][col].setColor(color);
+									pieceManager[prevRow][prevCol].setType("Empty");
+									pieceManager[prevRow][prevCol].setColor("White");
+									
+								}
 							}
+							setPiece();
+							availableBtn.clear();
 						}
     				}
 				});
@@ -92,14 +149,6 @@ public final class Board extends JPanel{
 		return board;
 	}
 	
-	/*
-	private static ImageIcon loadImage(String path){
-        Image image = new ImageIcon(Board.class.getResource(path)).getImage();
-        Image scaledImage = image.getScaledInstance(132, 132,  java.awt.Image.SCALE_SMOOTH);
-        return new ImageIcon(scaledImage);
-	}
-	*/
-	
 	/** Returns an ImageIcon, or null if the path was invalid. */
 	private static ImageIcon loadImage(String path) {
 	    java.net.URL imgURL = Board.class.getResource(path);
@@ -113,15 +162,12 @@ public final class Board extends JPanel{
 	
 	//initialize and display pieces 
 	private static void initialPosition(){
-		/*for(int i = 0; i < 14; i++){
-			pieceManager[i] = new Piece();
-		}*/
-		//initialize piece
-		//pieceManager[5][0] = pieceFactory.createPiece("Plus","Red");
-		//pieceManager[5][6] = pieceFactory.createPiece("Plus","Red");
-		//pieceManager[0][0] = pieceFactory.createPiece("Plus","Blue");
-		//pieceManager[0][6] = pieceFactory.createPiece("Plus","Blue");
 		
+		for (int r = 0; r < ROWS; r++) {
+			for (int c = 0; c < COLUMNS;  c++) {
+				pieceManager[r][c] = pieceFactory.createPiece("Empty", "White");
+			}
+		}
 		pieceManager[5][1] = pieceFactory.createPiece("Triangle","Red");
 		pieceManager[5][5] = pieceFactory.createPiece("Triangle","Red");
 		pieceManager[0][1] = pieceFactory.createPiece("Triangle","Blue");
@@ -132,26 +178,16 @@ public final class Board extends JPanel{
 		pieceManager[0][0] = pieceFactory.createPiece("Plus","Blue");
 		pieceManager[0][6] = pieceFactory.createPiece("Plus","Blue");
 		
+		pieceManager[5][2] = pieceFactory.createPiece("Chevron","Red");
+		pieceManager[5][4] = pieceFactory.createPiece("Chevron","Red");
+		pieceManager[0][2] = pieceFactory.createPiece("Chevron","Blue");
+		pieceManager[0][4] = pieceFactory.createPiece("Chevron","Blue");
 		
-		//display pieces
-		grid[5][0].setIcon(loadImage("RedPlus.png"));
-		grid[5][6].setIcon(loadImage("RedPlus.png"));
-		grid[0][0].setIcon(loadImage("BluePlus.png"));
-		grid[0][6].setIcon(loadImage("BluePlus.png"));
 		
-		grid[5][1].setIcon(loadImage("RedTriangle.png"));
-		grid[5][5].setIcon(loadImage("RedTriangle.png"));
-		grid[0][1].setIcon(loadImage("BlueTriangle.png"));
-		grid[0][5].setIcon(loadImage("BlueTriangle.png"));
-		
-		grid[5][2].setIcon(loadImage("RedChevron.png"));
-		grid[5][4].setIcon(loadImage("RedChevron.png"));
-		grid[0][2].setIcon(loadImage("BlueChevron.png"));
-		grid[0][4].setIcon(loadImage("BlueChevron.png"));
-		
-		grid[5][3].setIcon(loadImage("RedSun.png"));
-		grid[0][3].setIcon(loadImage("BlueSun.png"));	
-		//heloooooooooooooooooooooooooooo
+		pieceManager[0][3] = pieceFactory.createPiece("Sun","Blue");
+		pieceManager[5][3] = pieceFactory.createPiece("Sun","Red");
+
+		setPiece();
 		
 	}
 	private static void resetBoardColor(){
@@ -167,6 +203,55 @@ public final class Board extends JPanel{
 		}
 	
 	}
-
-
+	private static void setPiece()
+	{
+		for (int r = 0; r < ROWS; r++) 
+		{
+			for (int c = 0; c < COLUMNS;  c++) 
+			{
+				if (pieceManager[r][c].getType()=="Empty"){
+					grid[r][c].setIcon(null);
+					continue;
+				}
+				String color = pieceManager[r][c].getColor();
+				String type = pieceManager[r][c].getType();
+				String fileName = color+type+".png";
+				grid[r][c].setIcon(loadImage(fileName));
+			}
+		}
+	}
+    // Return player turn [Blue, Red]
+    public static String getPlayerTurn(){
+		if(turn % 2 == 0){
+        	return ("Blue");
+		}
+		else{
+        	return ("Red");
+       	} 
+	}
+	
+	// Return when player make a move
+	// To change the "Current turn" in the GameInfo
+	public static void setTurnFromBoard(){
+		GameInfo.changeCurrentTurn(getPlayerTurn());
+	}
+	
+	public static void printPM()
+	{
+		
+		System.out.println("--------------------------------------------");
+		for (int r = 0; r < ROWS; r++) 
+		{
+			for (int c = 0; c < COLUMNS;  c++) 
+			{
+				if (pieceManager[r][c].getType()=="Empty"){
+					continue;
+				}
+				String color = pieceManager[r][c].getColor();
+				String type = pieceManager[r][c].getType();
+				System.out.println(type+color+": "+r+","+c);
+			}
+		}
+		System.out.println("--------------------------------------------");
+	}
 }
