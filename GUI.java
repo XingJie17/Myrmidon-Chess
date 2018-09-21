27c4c1dd18617185de33eb62cbf6c9d2a0ddc5ec
Lim Cai Xian Lucinda <lcx.lucinda@gmail.com>
2018-09-21 18:04:30 +0800

    add newGame,update resign,update exit

--- GUI.java
+++ GUI.java
@@ -63,7 +63,17 @@ public class GUI{
 		
 		JMenu gameMenu = new JMenu("Game");
 		menuBar.add(gameMenu);
-		gameMenu.add(new JMenuItem("New Game"));
+		
+		gameMenu.add(new JMenuItem(new AbstractAction("New Game")
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				Board.initialPosition();	
+				GameInfo.newGame();
+			}
+			
+		}));
+		
 		gameMenu.add(new JMenuItem(new AbstractAction("Save Game"){
             public void actionPerformed(ActionEvent e){
                 JFileChooser fs = new JFileChooser(new File("c:\\"));
@@ -91,6 +101,7 @@ public class GUI{
                 }
             }
         }));
+		
         gameMenu.add(new JMenuItem(new AbstractAction("Load Game"){
             public void actionPerformed(ActionEvent e){
                 GameInfo.loadGame();
@@ -129,19 +140,23 @@ public class GUI{
                             break;
                     }
                     Board.initialPosition();
+	            GameInfo.newGame();
                 }
             }
         }));
 		
-		JMenuItem exitItem=new JMenuItem("Exit");
-		gameMenu.add(exitItem);
-		exitItem.addActionListener(new ActionListener(){
-			//Exit Game after click on "Exit" 
+		gameMenu.add(new JMenuItem(new AbstractAction("Exit")
+		{
 			public void actionPerformed(ActionEvent e)
 			{
-				System.exit(0);
+				JFrame f=new JFrame();
+				int n=JOptionPane.showConfirmDialog(f,"Would you like to exit the game?");
+				if(n==JOptionPane.YES_OPTION)
+				{
+					System.exit(0);
+				}
 			}
-		});
+		}));
 		
 		JMenu helpMenu = new JMenu("Help");
 		menuBar.add(helpMenu);
